a6d06002dd3368eeb1d594f440177c164948ca12
Carlos Rodriguez <2019030420@upsin.edu.mx>
2022-07-11 20:26:36 -0600

    modificacion para crear y actualizar alumnos

--- app/src/main/java/com/example/recycleviewapp/AlumnoAlta.java
+++ app/src/main/java/com/example/recycleviewapp/AlumnoAlta.java
@@ -1,9 +1,13 @@
 package com.example.recycleviewapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -42,6 +46,13 @@ public class AlumnoAlta extends AppCompatActivity {
             txtGrado.setText(alumno.getGrado());
             imgAlumno.setImageResource(alumno.getImg());
         }
+        imgAlumno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(getApplicationContext(), "Imagen", Toast.LENGTH_SHORT).show();
+                cargarImagen();
+            }
+        });
 
         btnGuardar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -93,5 +104,20 @@ public class AlumnoAlta extends AppCompatActivity {
         if(txtGrado.getText().toString().equals("")) exito = false;
 
         return exito;
+    }
+
+    private void cargarImagen(){
+        Intent intento = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intento.setType("image/");
+        startActivityForResult(intento.createChooser(intento, "Seleccione la aplicacion"),10);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode==RESULT_OK){
+            Uri path = data.getData();
+            imgAlumno.setImageURI(path);
+        }
     }
 }
